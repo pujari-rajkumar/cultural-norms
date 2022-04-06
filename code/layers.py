762d7752dedd736e59f07f1af3095cc2239abf09
Rajkumar Pujari <rpujari@purdue.edu>
2022-04-06 17:15:21 -0400

    adding code for relation prediction model

--- code/layers.py
+++ code/layers.py
@@ -1,14 +1,20 @@
+from sklearn.metrics import f1_score
+from sklearn.metrics import confusion_matrix
+from tqdm import tqdm
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.utils.data import DataLoader
 
 from transformers import *
 
 class RelationshipPredictor(nn.Module):
-
+    
     def __init__(self, args):
-        self.plm_model = args.plm_class.from_pretrained(args.plm_name)
+        super(RelationshipPredictor, self).__init__()
+        self.plm_model = BertModel.from_pretrained(args.plm_name)
         self.emotion_embedding = nn.Embedding(args.num_emotions, args.em_emb_dim)
         self.layers = []
         i = 0
@@ -20,20 +26,100 @@ class RelationshipPredictor(nn.Module):
             i += 1
         self.nl = nn.Tanh()
         self.position_layer = nn.Linear(args.sizes[-1], args.num_positions)
-        self.rleation_layer = nn.Linear(args.sizes[-1], args.num_relations)
+        self.relation_layer = nn.Linear(args.sizes[-1], args.num_relations)
         self.field_layer = nn.Linear(args.sizes[-1], args.num_fields)
-
-    def forward(self, ut, prev_ut, em, prev_em):
-        ut_emb = self.plm_model(ut['input_ids'].to(device), attention_mask=ut['attention_mask'].to(device))[1]
-        prev_ut_emb = self.plm_model(prev_ut['input_ids'].to(device), attention_mask=prev_ut['attention_mask'].to(device))[1]
-        em_emb = self.emotion_embedding(em)
-        prev_em_emb = self.emotion_embedding(prev_em)
+        self.f_loss_fn = nn.CrossEntropyLoss(weight=args.class_weights_f)
+        self.p_loss_fn = nn.CrossEntropyLoss(weight=args.class_weights_p)
+        self.r_loss_fn = nn.CrossEntropyLoss(weight=args.class_weights_r)
+        self.optimizer = AdamW(self.parameters(), lr=args.lr)
+        self.args = args
+    
+    def forward(self, batch):
+        with torch.no_grad():
+            ut_emb = self.plm_model(batch['ut_input_ids'], attention_mask=batch['ut_attention_mask'])[1]
+            prev_ut_emb = self.plm_model(batch['prev_ut_input_ids'], attention_mask=batch['prev_ut_attention_mask'])[1]
+        em_emb = self.emotion_embedding(batch['emotions'])
+        prev_em_emb = self.emotion_embedding(batch['prev_emotions'])
         x = torch.cat([ut_emb, prev_ut_emb, em_emb, prev_em_emb], dim=1)
         for layer in self.layers:
             x = self.nl(layer(x))
         position_out = F.softmax(self.position_layer(x), dim=1)
-        relation_out = F.softmax(self.relation_out(x), dim=1)
+        relation_out = F.softmax(self.relation_layer(x), dim=1)
         field_out = F.softmax(self.field_layer(x), dim=1)
-
+        
         return field_out, position_out, relation_out
-
+    
+    def get_results(self, batch_preds, batch_ys, batch_outs, loss_fn):
+        pred_y = torch.cat(batch_preds, dim=0)
+        data_y = torch.cat(batch_ys, dim=0)
+        pred_out = torch.cat(batch_outs, dim=0)
+        acc = sum((pred_y == data_y).float()) / data_y.size(0)
+        f1_mi = f1_score(data_y.cpu().data, pred_y.cpu().data, average='micro')
+        f1_ma = f1_score(data_y.cpu().data, pred_y.cpu().data, average='macro')
+        con_mat = confusion_matrix(data_y.cpu().data, pred_y.cpu().data)
+        val_loss = loss_fn(pred_out, data_y)
+        res = acc.cpu(), val_loss.cpu(), (f1_mi, f1_ma, con_mat)
+        return res
+    
+    def evaluate(self, data_loader):
+        self.eval()
+        batch_fouts = []
+        batch_fpreds = []
+        batch_fys = []
+        batch_pouts = []
+        batch_ppreds = []
+        batch_pys = []
+        batch_routs = []
+        batch_rpreds = []
+        batch_rys = []
+        for batch in tqdm(data_loader):
+            if self.args.use_cuda and torch.cuda.is_available():
+                with torch.cuda.device(self.args.cuda_device):
+                    batch = {key: val.cuda() for key, val in batch.items()}
+            batch_fout, batch_pout, batch_rout = self.forward(batch)
+            batch_fpred = torch.argmax(batch_fout, dim=1)
+            batch_ppred = torch.argmax(batch_pout, dim=1)
+            batch_rpred = torch.argmax(batch_rout, dim=1)
+            batch_fouts.append(batch_fout)
+            batch_fpreds.append(batch_fpred)
+            batch_fys.append(batch['fields'])
+            batch_pouts.append(batch_pout)
+            batch_ppreds.append(batch_ppred)
+            batch_pys.append(batch['positions'])
+            batch_routs.append(batch_rout)
+            batch_rpreds.append(batch_rpred)
+            batch_rys.append(batch['relations'])
+        res_f = self.get_results(batch_fpreds, batch_fys, batch_fouts, self.f_loss_fn)
+        res_p = self.get_results(batch_ppreds, batch_pys, batch_pouts, self.p_loss_fn)
+        res_r = self.get_results(batch_rpreds, batch_rys, batch_routs, self.r_loss_fn)
+        return (res_f, res_p, res_r)
+        
+    def train_model(self, train_loader, dev_loader, num_epochs=10, save_path='/homes/rpujari/scratch1_fortytwo/DARPA/saved_models/relation_model.pkl'):
+        max_val = -1
+        for epoch in tqdm(range(num_epochs)):
+            self.train()
+            loss_train_total = 0
+            progress_bar = tqdm(train_loader, desc='Epoch {:1d}'.format(epoch), leave=False, disable=False)
+            for batch in progress_bar:
+                if self.args.use_cuda and torch.cuda.is_available():
+                    with torch.cuda.device(self.args.cuda_device):
+                        batch = {key: val.cuda() for key, val in batch.items()}
+                labels = batch['relations']
+                field_out, position_out, relation_out = self.forward(batch)
+                reln_loss = self.r_loss_fn(relation_out, batch['relations'])
+                field_loss = self.f_loss_fn(field_out, batch['fields'])
+                pos_loss = self.p_loss_fn(position_out, batch['positions'])
+                loss = reln_loss + field_loss + pos_loss
+                loss_train_total += loss.to('cpu').item()
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+                
+            loss_train_avg = loss_train_total/len(train_loader)
+            tqdm.write(f'Training loss: {loss_train_avg}')
+                
+            dev_fres, dev_pres, dev_rres = self.evaluate(dev_loader)
+            dev_acc, dev_loss, (f1_mi, f1_ma, cm) = dev_rres
+            if f1_ma > max_val:
+                max_val = f1_ma
+                torch.save(self.state_dict(), save_path)   
--- code/train_relationship_prediction.py
+++ code/train_relationship_prediction.py
@@ -1,42 +1,44 @@
-import pickle
-import os
 import sys
-from datetime import datetime
-import threading
-import math
-import json
 import torch
 from transformers import *
-import re
-from nltk.corpus import stopwords
-import urllib
-from collections import Counter
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 import numpy as np
 import pandas as pd
-import torch
-from evaluation import *
 import argparse
 from collections import defaultdict
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
+from layers import RelationshipPredictor
+from collections import Counter
 
 parser = argparse.ArgumentParser(description='read arguments for training relationship prediction in MPDD')
 
-parser.add_argument('--dir_path', nargs='?', type=str, default='/u/zaphod_s3/mehta52/DARPA_project/', help='Where everything should be saved')
+parser.add_argument('--dir_path', nargs='?', type=str, default='/homes/rpujari/scratch1_fortytwo/DARPA/', help='Where everything should be saved')
 parser.add_argument('--train_model', action='store_true', help="True if you want to train the model, else it will only evaluate.")
-parser.add_argument('--do_eval_and_save', action='store_true', help="True if you want to evaluate the model and save the results.")
-parser.add_argument('--check_results', action='store_true', help="True if have already saved the results by running do_eval_and_save and now you want to run some analysis on them. For now we check the accuracy of each class for emotion classification.")
-parser.add_argument("--epoch_to_load", type=int, default=0, help="What epoch trained model you want to load")
-parser.add_argument("--num_epochs", type=int, default=1, help="How many epochs you want to run")
+parser.add_argument('--test_mode', action='store_true', help="True if you want to evaluate on test set.")
+parser.add_argument("--num_epochs", type=int, default=10, help="Number of epochs to train the model")
+parser.add_argument("--save_path", type=str, default='/homes/rpujari/scratch1_fortytwo/DARPA/saved_models/bert-base_relation_prediction.pt', help='path to the saved trained model parameters')
+parser.add_argument("--plm_name", type=str, default='hfl/chinese-bert-wwm', help='name of the transformers PLM to use')
+parser.add_argument("--num_emotions", type=int, default=7, help='number of emotions labels in the data')
+parser.add_argument("--em_emb_dim", type=int, default=30, help='size of emotion embedding to be used in the model')
+parser.add_argument("--sizes", type=list, default=[1596, 1000, 20], help='sizes of feed forward layers of the model')
+parser.add_argument("--num_positions", type=int, default=4, help='number of position labels in the data')
+parser.add_argument("--num_relations", type=int, default=25, help='number of relation labels in the data')
+parser.add_argument("--num_fields", type=int, default=5, help='number of field labels in the data')
+parser.add_argument('--use_cuda', action='store_true', help="true is you want to use GPU")
+parser.add_argument("--cuda_device", type=int, default=0, help='GPU to be used for the model training')
+parser.add_argument("--lr", type=float, default=5e-5, help="learning rate for the model training")
+parser.add_argument("--class_weights_f", type=list, default=[1.] * 5, help='loss fn weights for each field class, they will be computed from training data')
+parser.add_argument("--class_weights_p", type=list, default=[1.] * 4, help='loss fn weights for each position class, they will be computed from training data')
+parser.add_argument("--class_weights_r", type=list, default=[1.] * 25, help='loss fn weights for each relation class, they will be computed from training data')
 
 args = parser.parse_args()
 
 def get_data(given_dataset):
     '''helper function to process the pandas dataframe for the MPDD dataset'''
 
-    utteraces = []
+    utterances = []
     prev_utterances = []
     emotions = []
     prev_emotions = []
@@ -52,13 +54,13 @@ def get_data(given_dataset):
         fields.append(row['field'])
         positions.append(row['position'])
         relations.append(row['relation'])
-        
+
     return utterances, prev_utterances, emotions, prev_emotions, fields, positions, relations
 
 
 class CustomDataset(torch.utils.data.Dataset):
     '''class for our customDataset that we can pass into PyTorch via dataloader'''
-    def __init__(self, ut, prev_ut, em, prev_em, fs, ps, rs, task='relationship'):
+    def __init__(self, ut, prev_ut, em, prev_em, fs, ps, rs, em_ls, r_ls, p_ls, f_ls):
         self.utterance_encs = ut
         self.prev_utterance_encs = prev_ut
         self.emotions = em
@@ -66,20 +68,24 @@ class CustomDataset(torch.utils.data.Dataset):
         self.fields = fs
         self.positions = ps
         self.relationships = rs
-        self.task = task
+        self.emotion_labels = em_ls
+        self.relation_labels = r_ls
+        self.positions_labels = p_ls
+        self.field_labels = f_ls
 
     def __getitem__(self, idx):
-        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
-        if self.task == 'relationship':
-            item['labels'] = torch.tensor(self.relationships[idx])
-        elif self.task = 'position':
-            item['labels'] = torch.tensor(self.positions[idx])
-        elif self.task == 'field':
-            item['labels'] = torch.tensor(self.fields[idx])
+        item = {'ut_' + key: torch.tensor(val[idx]) for key, val in self.utterance_encs.items()}
+        item.update({'prev_ut_' + key: torch.tensor(val[idx]) for key, val in self.prev_utterance_encs.items()})
+        item['emotions'] = torch.tensor(self.emotion_labels[self.emotions[idx]])
+        item['prev_emotions'] = torch.tensor(self.emotion_labels[self.prev_emotions[idx]])
+        item['relations'] = torch.tensor(self.relation_labels[self.relationships[idx]])
+        item['positions'] = torch.tensor(self.positions_labels[self.positions[idx]])
+        item['fields'] = torch.tensor(self.field_labels[self.fields[idx]])
         return item
 
     def __len__(self):
         return len(self.relationships)
+
 
 def load_dict(dict_path):
     '''helper function to load a dictionary given a dict_path'''
@@ -96,9 +102,33 @@ test_csv = pd.read_csv(args.dir_path + 'relationship_test.tsv', sep='\t')
 
 # process the data 
 tr_ut, tr_prev_ut, tr_em, tr_prev_em, tr_f, tr_p, tr_r = get_data(train_csv)
+f_counts = Counter(tr_f)
+p_counts = Counter(tr_p)
+r_counts = Counter(tr_r)
+
+print('Train data class distribution counts')
+print(f_counts)
+print(p_counts)
+print(r_counts)
+
 de_ut, de_prev_ut, de_em, de_prev_em, de_f, de_p, de_r = get_data(dev_csv)
 te_ut, te_prev_ut, te_em, te_prev_em, te_f, te_p, te_r = get_data(test_csv)
 
+#load label to int mappings
+reln_em = load_dict(args.dir_path + 'relationship_emotion-label_dict.npy')
+reln_r = load_dict(args.dir_path + 'relationship_relation-label_dict.npy')
+reln_p = load_dict(args.dir_path + 'relationship_position-label_dict.npy')
+reln_f = load_dict(args.dir_path + 'relationship_field-label_dict.npy')
+for field in f_counts:
+    args.class_weights_f[reln_f[field]] = 1.0 / f_counts[field]
+for position in p_counts:
+    args.class_weights_p[reln_p[position]] = 1.0 / p_counts[position]
+for relation in r_counts:
+    args.class_weights_r[reln_r[relation]] = 1.0 / r_counts[relation]
+
+args.class_weights_f = torch.tensor(args.class_weights_f)
+args.class_weights_p = torch.tensor(args.class_weights_p)
+args.class_weights_r = torch.tensor(args.class_weights_r)
 
 # set up the tokenizer and tokenize the data
 tokenizer_class = BertTokenizer
@@ -110,13 +140,13 @@ tr_prev_ut_enc = tokenizer(tr_prev_ut, truncation=True, padding=True, return_att
 de_ut_enc = tokenizer(de_ut, truncation=True, padding=True, return_attention_mask=True, pad_to_max_length=True, max_length=512)
 de_prev_ut_enc = tokenizer(de_prev_ut, truncation=True, padding=True, return_attention_mask=True, pad_to_max_length=True, max_length=512)
 
-tr_ut_enc = tokenizer(te_ut, truncation=True, padding=True, return_attention_mask=True, pad_to_max_length=True, max_length=512)
+te_ut_enc = tokenizer(te_ut, truncation=True, padding=True, return_attention_mask=True, pad_to_max_length=True, max_length=512)
 te_prev_ut_enc = tokenizer(te_prev_ut, truncation=True, padding=True, return_attention_mask=True, pad_to_max_length=True, max_length=512)
 
 # set up the data as a dataset
-train_dataset = CustomDataset(tr_ut_enc, tr_prev_ut_enc, tr_em, tr_prev_em, tr_f, tr_p, tr_r)
-dev_dataset = CustomDataset(de_ut_enc, de_prev_ut_enc, de_em, de_prev_em, de_f, de_p, de_r)
-test_dataset = CustomDataset(te_ut_enc, te_prev_ut_enc, te_em, te_prev_em, te_f, te_p, te_r)
+train_dataset = CustomDataset(tr_ut_enc, tr_prev_ut_enc, tr_em, tr_prev_em, tr_f, tr_p, tr_r, reln_em, reln_r, reln_p, reln_f)
+dev_dataset = CustomDataset(de_ut_enc, de_prev_ut_enc, de_em, de_prev_em, de_f, de_p, de_r, reln_em, reln_r, reln_p, reln_f)
+test_dataset = CustomDataset(te_ut_enc, te_prev_ut_enc, te_em, te_prev_em, te_f, te_p, te_r, reln_em, reln_r, reln_p, reln_f)
 
 # set up the dataloader
 train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
@@ -124,84 +154,26 @@ dev_loader = DataLoader(dev_dataset, batch_size=64, shuffle=False)
 test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
 
 # set up the model
-model_class = BertForSequenceClassification
-config_class = BertConfig
-model = model_class.from_pretrained('hfl/chinese-bert-wwm', num_labels=len(Counter(train_labels)) + 1)
-device = torch.device("cuda")
-model.to(device)
+model = RelationshipPredictor(args)
+if args.use_cuda and torch.cuda.is_available():
+    with torch.cuda.device(args.cuda_device):
+        model.cuda()
+        model.load_state_dict(torch.load(args.save_path))
 
-print("Training the model")
-sys.stdout.flush()
-
-optim = AdamW(model.parameters(), lr=5e-5)
 
 if args.train_model:
-    best_val_f1 = 0.0
-    label_dict = load_dict(args.dir_path + 'label_dict.npy')
-    for epoch in tqdm(range(args.num_epochs)):
-
-        model.train()
-
-        loss_train_total = 0
-    
-        progress_bar = tqdm(train_loader, desc='Epoch {:1d}'.format(epoch), leave=False, disable=False)
-        for batch in progress_bar:
-            optim.zero_grad()
-            input_ids = batch['input_ids'].to(device)
-            attention_mask = batch['attention_mask'].to(device)
-            labels = batch['labels'].to(device)
-            outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
-            loss = outputs[0]
-            loss_train_total += loss.item()
-            loss.backward()
-            optim.step()
-
-        loss_train_avg = loss_train_total/len(train_loader)            
-        tqdm.write(f'Training loss: {loss_train_avg}')
-
-        val_loss, predictions, true_vals = evaluate(model, val_loader, device)
-        val_f1 = f1_score_func(predictions, true_vals)
-        tqdm.write(f'Validation loss: {val_loss}')
-        tqdm.write(f'F1 Score (Weighted): {val_f1}')
-        accuracy_per_class(preds=predictions, labels=true_vals, label_dict=label_dict)
-
-        if val_f1 > best_val_f1:
-            best_val_f1 = val_f1
-            print("Best validation F1 so far is " + str(best_val_f1))
-
-        torch.save(model.state_dict(), f'/u/zaphod_s3/mehta52/DARPA_project/saved_models/finetuned_BERT_epoch_{epoch}.model')
-
-elif args.do_eval_and_save:
-
-    model.load_state_dict(torch.load(args.dir_path + '/saved_models/finetuned_BERT_epoch_' + str(args.epoch_to_load) + '.model', map_location=torch.device('cpu')))
-
-    model.eval()
-    
-    # evaluate the model and save the predictions so we can further analyze it later
-    print("Evaluating the model")
+    print("Training the model")
     sys.stdout.flush()
-    val_loss, predictions, true_vals = evaluate(model, val_loader, device)
-    np.save(args.dir_path + '/results_emotion/val_predictions.npy', np.asarray(predictions))
-    np.save(args.dir_path + '/results_emotion/val_true_values.npy', np.asarray(true_vals))
-    val_f1 = f1_score_func(predictions, true_vals)
+    model.train_model(train_loader, dev_loader, num_epochs=args.num_epochs, save_path=args.save_path)
 
-    test_loss, test_predictions, test_vals = evaluate(model, test_loader, device)
-    np.save(args.dir_path + '/results_emotion/test_predictions.npy', np.asarray(test_predictions))
-    np.save(args.dir_path +  '/results_emotion/test_true_values.npy', np.asarray(test_vals))
-    test_f1 = f1_score_func(preds=test_predictions, labels=test_vals)
-    print("Test f1 is " + str(test_f1))
+if args.test_mode:
 
-    label_dict = load_dict(args.dir_path + 'label_dict.npy')
-
-    accuracy_per_class(preds=test_predictions, labels=test_vals, label_dict=label_dict)
-
-elif args.check_results:
-
-    # check the F1 score of each class
-    val_predictions = np.load(args.dir_path + '/results_emotion/val_predictions.npy')
-    val_true_values = np.load(args.dir_path + '/results_emotion/val_true_values.npy')
-
-    # load the label dict so we can map the classes. This was computed in setup_data.py
-    label_dict = load_dict(args.dir_path + 'label_dict.npy')
-
-    accuracy_per_class(preds=val_predictions, labels=val_true_values, label_dict=label_dict)
+    model.load_state_dict(torch.load(args.save_path))
+    dev_res = model.evaluate(dev_loader)
+    print(dev_res[0])
+    print(dev_res[1])
+    print(dev_res[2])
+    test_res = model.evaluate(test_loader)
+    print(test_res[0])
+    print(test_res[1])
+    print(test_res[2])
